1aaed703bd6b41550a03b8d1e45eb9c70430c606
Annalisa Nasca <annalisanasca06@gmail.com>
2024-03-05 09:54:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import reactLogo from "./assets/react.svg";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </div>
+        <h1>ANNALISA NASCA</h1>
+
+        <p className="read-the-docs">Coming Soon..</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
